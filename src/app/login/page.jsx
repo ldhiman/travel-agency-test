cc4435ccf7e5e1057c4969d839dfa9400b2f6f5a
ldhiman <ldhiman.597@gmail.com>
2024-08-04 11:40:48 +0530

    OTP functinality

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,40 +1,86 @@
-'use client'
+"use client";
 
-// LoginPage.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
-import { auth, db, setupRecaptcha, signInWithPhoneNumber } from "../firebase";
+import toast from "react-hot-toast";
+import {
+  auth,
+  db,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  PhoneAuthProvider,
+  signInWithCredential,
+} from "../firebase";
 import { ref, set } from "firebase/database";
 
 export default function LoginPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [otp, setOtp] = useState("");
   const [verificationId, setVerificationId] = useState(null);
+  const [otpSent, setOtpSent] = useState(false);
   const router = useRouter();
+  const recaptchaContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && recaptchaContainerRef.current) {
+      if (window.recaptchaVerifier) {
+        window.recaptchaVerifier.clear();
+      }
+
+      const recaptchaVerifier = new RecaptchaVerifier(
+        auth,
+        recaptchaContainerRef.current,
+        {
+          size: "invisible",
+          callback: () => {
+            console.log("reCAPTCHA resolved.");
+          },
+        }
+      );
+      window.recaptchaVerifier = recaptchaVerifier;
+    }
+  }, [recaptchaContainerRef]);
 
   const requestOtp = async () => {
-    setupRecaptcha();
     const appVerifier = window.recaptchaVerifier;
     try {
-      const confirmationResult = await signInWithPhoneNumber(
+      const confirmationResult = signInWithPhoneNumber(
         auth,
-        phoneNumber,
+        "+91" + phoneNumber,
         appVerifier
       );
-      setVerificationId(confirmationResult.verificationId);
-      console.log("OTP sent");
+
+      toast.promise(confirmationResult, {
+        loading: "Sending OTP...",
+        success: () => {
+          setVerificationId(confirmationResult.verificationId);
+          setOtpSent(true);
+          return "OTP sent successfully!";
+        },
+        error: (error) => {
+          if (error.code === "auth/too-many-requests") {
+            return "Too many requests. Please wait a few minutes before trying again.";
+          }
+          console.log(error);
+          return "An error occurred. Please try again.";
+        },
+      });
     } catch (error) {
       console.error("Error during signInWithPhoneNumber:", error);
+      toast.error("An error occurred. Please try again.");
     }
   };
 
   const verifyOtp = async () => {
-    const credential = firebase.auth.PhoneAuthProvider.credential(
-      verificationId,
-      otp
-    );
+    if (!verificationId) {
+      toast.error("Verification ID is missing.");
+      console.error("Verification ID is missing");
+      return;
+    }
+
+    const credential = PhoneAuthProvider.credential(verificationId, otp);
     try {
-      const userCredential = await auth.signInWithCredential(credential);
+      const userCredential = await signInWithCredential(auth, credential);
       const user = userCredential.user;
 
       await set(ref(db, "users/" + user.uid), {
@@ -42,9 +88,10 @@ export default function LoginPage() {
         type: "customer",
       });
 
-      console.log("User logged in successfully");
+      toast.success("User logged in successfully.");
       router.push("/register");
     } catch (error) {
+      toast.error("Error during OTP verification: " + error.message);
       console.error("Error during OTP verification:", error);
     }
   };
@@ -60,36 +107,50 @@ export default function LoginPage() {
             <label className="font-semibold text-sm text-gray-600 pb-1 block">
               Mobile Number
             </label>
-            <input
-              type="text"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              placeholder="Phone number"
-            />
+            <div className="flex items-center border rounded-lg mb-5">
+              <span className="px-3 py-2 text-gray-600 bg-gray-200 border-r">
+                +91
+              </span>
+              <input
+                type="text"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                className="flex-1 px-3 py-2 text-sm outline-none"
+                placeholder="Phone number"
+              />
+            </div>
+            <div id="recaptcha-container" ref={recaptchaContainerRef}></div>
             <button
               onClick={requestOtp}
-              className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+              className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+            >
               Send OTP
             </button>
-            <label className="font-semibold text-sm text-gray-600 pb-1 block">
-              Verify OTP
-            </label>
-            <input
-              type="text"
-              value={otp}
-              onChange={(e) => setOtp(e.target.value)}
-              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              placeholder="OTP"
-            />
-            <button
-              onClick={verifyOtp}
-              className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
-              Submit OTP
-            </button>
+
+            {otpSent && (
+              <>
+                <label className="font-semibold text-sm text-gray-600 pb-1 block">
+                  Verify OTP
+                </label>
+                <input
+                  type="text"
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  placeholder="OTP"
+                />
+                <button
+                  onClick={verifyOtp}
+                  className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                >
+                  Submit OTP
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
+      <div id="toast-container" />
     </div>
   );
 }
