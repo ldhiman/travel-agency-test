28bc3761618d39792a2cd18b4c21b66ead2c1101
Abhishek <100993983+Abhishek-Jatav@users.noreply.github.com>
2024-11-01 10:41:16 +0530

    Update ReviewSection.jsx

--- src/app/components/ReviewSection.jsx
+++ src/app/components/ReviewSection.jsx
@@ -39,27 +39,28 @@ const FeatureCard = ({ image, title, description, link }) => (
 );
 
 export default function ReviewSection() {
+  
   const features = [
     {
       image: "/hill.jpg",
       title: "Exciting Packages",
       description:
         "Discover our curated travel packages for unforgettable experiences.",
-      link: "/packages",
+      link: "/",
     },
     {
       image: "/driver.jpg",
       title: "Professional Drivers",
       description:
         "Meet our experienced and courteous drivers for a safe journey.",
-      link: "/drivers",
+      link: "/",
     },
     {
       image: "/cars.jpg",
       title: "Premium Vehicles",
       description:
         "Choose from our fleet of comfortable and well-maintained vehicles.",
-      link: "/vehicles",
+      link: "/",
     },
   ];
 
